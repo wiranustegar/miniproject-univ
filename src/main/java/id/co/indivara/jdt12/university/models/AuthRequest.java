126c201a24382336fae975f7876c18be64c0fd96
wiranust <wrns.tegar@gmail.com>
2023-06-28 07:00:07 +0700

    add login feature with JWT

---
+++ src/main/java/id/co/indivara/jdt12/university/models/AuthRequest.java
@@ -0,0 +1,14 @@
+package id.co.indivara.jdt12.university.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String userName;
+    private String password;
+}
--- src/main/java/id/co/indivara/jdt12/university/security/SpringSecurityConfig.java
+++ src/main/java/id/co/indivara/jdt12/university/security/SpringSecurityConfig.java
@@ -1,54 +1,52 @@
 package id.co.indivara.jdt12.university.security;
 
+import id.co.indivara.jdt12.university.filter.JwtFilter;
+import id.co.indivara.jdt12.university.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("student").password("{noop}inistudent").roles("STUDENT")
-                .and()
-                .withUser("lecturer").password("{noop}inilecturer").roles("LECTURER")
-                .and()
-                .withUser("admin").password("{noop}iniadmin").roles("STUDENT", "LECTURER", "ADMIN");
+        auth.userDetailsService(userDetailsService);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests()
-                //ADMIN ONLY ROLE
-                .antMatchers(HttpMethod.POST, "/report/register/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/report/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/classroom/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/classroom/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/classroom/{classroomId}/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/student/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/student/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/student/{studentId}/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/student/{studentId}/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/lecturer/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/lecturer/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/lecturer/{lecturerId}/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/lecturer/{lecturerId}/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/subject/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/subject/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/subject/{subjectId}/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/subject/{subjectId}/").hasRole("ADMIN")
-
-                //LECTURER ROLE
-                .antMatchers(HttpMethod.PUT, "/report/input-record/").hasRole("LECTURER")
-                .antMatchers(HttpMethod.GET, "/report/{classroomId}/").hasRole("LECTURER")
-                .antMatchers(HttpMethod.GET, "/report/student/{classroomId}/").hasRole("LECTURER")
-                .antMatchers(HttpMethod.GET, "/lecturer/{lecturerId}/").hasRole("LECTURER")
-
-                //STUDENT ROLE
-                .antMatchers(HttpMethod.GET, "/student/{studentId}/").hasRole("STUDENT")
-                .and()
-                .csrf().disable()
-                .formLogin().disable();
+        http.csrf().disable().authorizeRequests().antMatchers("/report/authenticate")
+                .permitAll().anyRequest().authenticated()
+                .and().exceptionHandling().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
     }
 }
